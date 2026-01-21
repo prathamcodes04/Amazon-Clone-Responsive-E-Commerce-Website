38ec9cfafb3569f9fa053d648880acb4fc49a290
Prat <prathamgupt0405@gmail.com>
2026-01-21 19:49:13 +0530

    Enhance cart item removal by targeting specific containers in the DOM

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -22,7 +22,7 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML +=  `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -111,6 +111,10 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
   link.addEventListener('click', () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
+    
+    //getting specific container we need to remove using id
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    
+    container.remove(); //removes container from page
   });
 });
