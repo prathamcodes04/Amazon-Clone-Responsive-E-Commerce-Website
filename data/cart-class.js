8910dde7b2077a14afe6bf631a32a8ec1c55cf70
Pratham Gupta <prathamgupta0405@gmail.com>
2026-01-24 03:23:31 +0530

    Refactor Cart class to use private fields for localStorageKey and improve encapsulation

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,14 +1,14 @@
 class Cart {
   cartItems;   // cartItems = undefined;
-  localStorageKey;
+  #localStorageKey;
 
   constructor(localStorageKey) {
-    this.localStorageKey = localStorageKey;
-    this.loadFromStorage();
+    this.#localStorageKey = localStorageKey;
+    this.#loadFromStorage();
   }
 
-  loadFromStorage() {
-    this.cartItems = JSON.parse(localStorage.getItem("this.localStorageKey"));
+  #loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem("this.#localStorageKey"));
 
     if (!this.cartItems) {
       this.cartItems = [
@@ -27,7 +27,7 @@ class Cart {
   }
 
   saveToStorage() {
-    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+    localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
   }
 
   addToCart(productId) {
@@ -83,6 +83,8 @@ class Cart {
 
 const cart = new Cart('cart-oop');
 const buisnessCart = new Cart('cart-buisness');
+
+
 
 console.log(cart);
 console.log(buisnessCart);
